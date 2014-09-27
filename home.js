279c1619fbded60ab458411bcbd37e93ecc21256
ldd <leonardoldd@gmail.com>
2014-09-27 13:20:10 -0400

    small fixes, more info, proper css

--- home.js
+++ home.js
@@ -1,34 +1,34 @@
 //JSON representing the gym leader's data
 var data;
-	var url = "http://www.evolvecraft.com/ajax.php?s=minecraft&f=players-online-avatars&preset_id=18527811&server=combined&format=json?callback=?";
-	url = 'http://query.yahooapis.com/v1/public/yql?q=' + encodeURIComponent('select * from html where url="' + url + '"') + '&format=xml';
+var url = "http://www.evolvecraft.com/ajax.php?s=minecraft&f=players-online-avatars&preset_id=18527811&server=combined&format=json?callback=?";
+url = 'http://query.yahooapis.com/v1/public/yql?q=' + encodeURIComponent('select * from html where url="' + url + '"') + '&format=xml';
 
-  function ajaxGetNames(wrapper, regionClass, regionText, gymClass, gymText){
-  var cities = initializeCities(wrapper, regionClass, regionText, gymClass, gymText);
-  $.ajax({
-	dataType: "text",
-	url: url,
-	success: function(text){
+function ajaxGetNames(wrapper, regionClass, regionText, gymClass, gymText){
+	var cities = initializeCities(wrapper, regionClass, regionText, gymClass, gymText);
+	$.ajax({
+		dataType: "text",
+		url: url,
+		success: function(text){
+			//parse XML response which contains all names
 			var xmldoc = $.parseXML(text);
 			var names = $(xmldoc).find('body');
-			console.log("done");
 
 			for (index = 0; index < names.children().length; index++){
-				var nameImage = names.children()[index].innerHTML;
+				var nameImage  = names.children()[index].innerHTML;
+
+				//get usersName and nameImage using a hack
 				var nameString = nameImage.split('/')[4];
-				nameImage = nameImage.replace('alt=""', 'title=' + nameString);
+				nameImage      = nameImage.replace('alt=""', 'title=' + nameString);
 				
 				if (cities.hasOwnProperty(nameString)){
 
-          for (var i = 0; i < cities[nameString].length; i++) {
-           var city = "#" + cities[nameString][i];
+					for (var i = 0; i < cities[nameString].length; i++) {
+						var city = "#" + cities[nameString][i];
 
-            $(city).append(nameImage);
-            $(city).show();
-          };
+						$(city).append(nameImage);
+						$(city).show();
+					};
 				}
-
-				
 			}
 		}
 	});
@@ -36,63 +36,62 @@ var data;
 
   var interval = 1000 * 60 * 5; //5 minutes
   function start(jsonFile, wrapper, regionClass, regionText, gymClass, gymText){
-  $.getJSON(jsonFile, function( rawData){
-  data = rawData;
-ajaxGetNames(wrapper, regionClass, regionText, gymClass, gymText);
-
-});
-	  setInterval(function(){ajaxGetNames(wrapper, regionClass, regionText, gymClass, gymText);}, interval);
+  	$.getJSON(jsonFile, function( rawData){
+  		data = rawData;
+  		ajaxGetNames(wrapper, regionClass, regionText, gymClass, gymText);
+  	});
+  	setInterval(function(){ajaxGetNames(wrapper, regionClass, regionText, gymClass, gymText);}, interval);
   }
 
-	function initializeCities(wrapper, regionClass, regionText, gymClass, gymText){
-		var wrapper = wrapper || "#wrapper";
-		var regionClass = regionClass || "col-md-4";
-		var regionText = regionText || " Gym Leaders";
-		var gymClass = gymClass || '';
-		var gymText = gymText || " Gym: ";
+  function initializeCities(wrapper, regionClass, regionText, gymClass, gymText){
+  	var wrapper = wrapper || "#wrapper";
+  	var regionClass = regionClass || "col-md-4";
+  	var regionText = regionText || " Gym Leaders";
+  	var gymClass = gymClass || '';
+  	var gymText = gymText || " Gym: ";
 
-    $(wrapper).html("");
-		var cities = new Object();
-		for (regionName in data.regions){
+  	$(wrapper).html("");
+  	var cities = new Object();
+  	for (regionName in data.regions){
 
-			$(wrapper).append('<div class="'+ regionClass +'" id="'+regionName+'"><h3>'+regionName+ regionText+'</h3></div>')
+  		$(wrapper).append('<div class="'+ regionClass +'" id="'+regionName+'"><h3>'+regionName+ regionText+'</h3></div>')
 
-			for (n = 0; n < data.regions[regionName].cities.length; n++){
-				for (key in data.regions[regionName].cities[n]){
+  		for (n = 0; n < data.regions[regionName].cities.length; n++){
+  			for (key in data.regions[regionName].cities[n]){
 
-			$("#" + regionName).append('<div class="' + gymClass +'" id="'+key+'">'+ "#"+ (n+1) +  key+ gymText + '<p></p></div>')
-			$('#' + key).hide();
+  				$("#" + regionName).append('<div class="' + gymClass +'" id="'+key+'">'+ "#"+ (n+1) +  " " + key+ gymText + '<p></p></div>')
+  				$('#' + key).hide();
 
-					for (i =0 ; i < data.regions[regionName].cities[n][key].leaders.length; i++){
-						var name = data.regions[regionName].cities[n][key].leaders[i];
+  				for (i =0 ; i < data.regions[regionName].cities[n][key].leaders.length; i++){
+  					var name = data.regions[regionName].cities[n][key].leaders[i];
 
-			            if (cities[name] == undefined){
-			              cities[name] = [key];
-			            }
-			            else{
-			              cities[name].push(key);
-			            }
-					}
+  					if (cities[name] == undefined){
+  						cities[name] = [key];
+  					}
+  					else{
+  						cities[name].push(key);
+  					}
+  				}
 
-					var format   = data.regions[regionName].cities[n][key].format || "(none)";
-					var levelCap = data.regions[regionName].cities[n][key].levelCap || "(none)";
+  				var format   = data.regions[regionName].cities[n][key].format || "(none)";
+  				var levelCap = data.regions[regionName].cities[n][key].levelCap || "(none)";
 
-					clickAction("#" + key, "p", "Format: " + format + " Level Cap:" + levelCap);
-
-
-				}
-			}
-		}
-
-    return cities;
-    };
+  				clickAction("#" + key, "p", "Format: " + format + " Level Cap:" + levelCap);
 
 
-    function clickAction(id, sub, text){
-    	$(id).click(function(){
-    		if ($(this).find(sub).text().length == 0)
-    			$(id).find(sub).text(text);
-    		else
-    			$(id).find(sub).text('');
-    	});
-    }
+  			}
+  		}
+  	}
+
+  	return cities;
+  };
+
+
+  function clickAction(id, sub, text){
+  	$(id).click(function(){
+  		if ($(this).find(sub).text().length == 0)
+  			$(id).find(sub).text(text);
+  		else
+  			$(id).find(sub).text('');
+  	});
+  }
